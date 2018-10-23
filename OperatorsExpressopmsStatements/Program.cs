d5370924ea8b272e64b1d22ee927992a267e0c97
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-10-23 15:56:18 -0700

    first push

---
+++ OperatorsExpressopmsStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorsExpressopmsStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
